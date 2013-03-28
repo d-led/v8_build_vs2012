6b75b6f5c6ad69f356d6a39215a8b2b5f0e58489
d-led <dmlled@yahoo.com>
2013-03-28 23:26:17 +0100

    optimized xcopy, debug, release

--- package_v8_lib.bat
+++ package_v8_lib.bat
@@ -1,7 +1,8 @@
 mkdir v8_lib
 mkdir v8_lib\lib
 mkdir v8_lib\include
-xcopy v8\build\Release\lib\* v8_lib\lib
+xcopy v8\build\Release\lib\* v8_lib\lib\Release\
+xcopy v8\build\Debug\lib\* v8_lib\lib\Debug\
 xcopy v8\include\* v8_lib\include
 xcopy v8\* v8_lib
 pause
