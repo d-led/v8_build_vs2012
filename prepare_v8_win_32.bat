ae3de2883a593e212c2b1bf7e307b0196d75f0d0
d-led <dmlled@yahoo.com>
2013-03-28 23:07:21 +0100

    windows path char

--- prepare_v8_win_32.bat
+++ prepare_v8_win_32.bat
@@ -3,4 +3,4 @@ cd v8
 svn co http://gyp.googlecode.com/svn/trunk build/gyp
 svn co http://src.chromium.org/svn/trunk/tools/third_party/python_26@89111 third_party/python_26
 svn co http://src.chromium.org/svn/trunk/deps/third_party/cygwin@66844 third_party/cygwin
-third_party/python_26/python.exe build\gyp_v8
+third_party\python_26\python.exe build\gyp_v8
